a3aad4c93e1cd810f96e2cb913ba0923cb004141
Averion <admin@averionbot.com>
2026-06-11 05:18:46 +0000

    feat: scalper tab open trades column

--- api.py
+++ api.py
@@ -1412,6 +1412,3 @@ def health_check():
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8080)
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
